e35b8e53e62210d9846be1b7b9f750384bbe328f
Timothy Do <timothy.do@gmail.com>
2016-06-27 20:42:47 -0700

    Player Collision implemented

---
+++ Assets/Client Assets/Scenes/TDo/PlayerCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider other) {
+    Destroy(other.transform.parent.gameObject);
+    //Send emit for death to network
+  }
+}

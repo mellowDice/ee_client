d3a4553bd686284c8d02ed59037923e407a32cb3
Timothy Do <timothy.do@gmail.com>
2016-06-29 23:00:22 -0700

    Fixing reticle for VR

---
+++ Assets/Client Assets/Scripts/Reticle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reticle : MonoBehaviour {
+  public Camera mainCam;
+  private Vector3 scale;
+
+  void Start () {
+  	scale = transform.localScale;	
+  }
+
+  void Update () {
+	float distance;
+	RaycastHit hit = new RaycastHit ();
+	if (Physics.Raycast (new Ray (mainCam.transform.position, mainCam.transform.rotation * Vector3.forward), out hit) && (hit.collider.name != "Player")) {
+		distance = hit.distance;
+	} else { 
+	  distance = mainCam.farClipPlane * .95f;
+	}
+	transform.position = mainCam.transform.position + mainCam.transform.rotation * Vector3.forward * distance;
+	transform.LookAt (mainCam.transform.position);
+	transform.Rotate (0f, 180f, 0f);
+	transform.localScale = scale * distance;
+
+  }
+}

b025b9b84f3ad0a4b84d9411d9dadc614a369e94
Austin Baltes <austinba@gmail.com>
2016-07-05 16:57:13 -0700

    tweaking

--- Assets/Client Assets/Scripts/PlayerCollision.cs
+++ Assets/Client Assets/Scripts/PlayerCollision.cs
@@ -19,12 +19,9 @@ public class PlayerCollision : MonoBehaviour {
   //COLLISION CHECKER//
   /////////////////////
 	void OnTriggerEnter(Collider other) {
-      Debug.Log("---COLLISION--- " + other);
-
     if (other.gameObject.CompareTag("Zombie") || other.gameObject.CompareTag("Player")) {
       var otherPlayer = other.transform.parent.gameObject;
       var otherPlayerAttributes = otherPlayer.GetComponent<PlayerAttributes>();
-      Debug.Log("---COLLISION--- " + otherPlayerAttributes.id);
       var playerAttributes = GetComponent<PlayerAttributes>();
 
       // If main player is smaller
@@ -35,7 +32,8 @@ public class PlayerCollision : MonoBehaviour {
       // If other player is smaller
       else if (otherPlayerAttributes.playerMass < playerAttributes.playerMass){
         PlayerNetworkController.Kill(otherPlayerAttributes.id, other.GetComponent<Transform>().position.x, other.GetComponent<Transform>().position.z);
-        if(GameAttributes.mainPlayer == gameObject) particles.Play();
+        Debug.Log(GameAttributes.mainPlayer.GetComponent<PlayerAttributes>().id + " " + playerAttributes.id);
+        if(GameAttributes.mainPlayer.GetComponent<PlayerAttributes>().id == playerAttributes.id) particles.Play();
         if (other.gameObject.CompareTag("Zombie")) {
           ZombieSpawner.ZombieCollide(other.transform.parent.gameObject);
         }

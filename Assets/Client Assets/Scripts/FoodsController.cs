3ce7a3d5f315dc5a8e5a874c76138db7f142399a
aaronwbrown <aaron.w.brown13@gmail.com>
2016-07-06 18:56:53 -0700

    Add Raycast to objects

---
+++ Assets/Client Assets/Scripts/FoodsController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class FoodsController : MonoBehaviour {
+
+  public GameObject foodPrefab;
+  bool objectState;
+  Dictionary<string, GameObject> foodsDict = new Dictionary<string, GameObject>();
+
+  public void CreateFood (JSONObject foods)
+  {
+
+
+    var length = foods.list.Count;
+    for (var i = 0; i < length; i++) {
+      var id = foods[i]["id"].ToString();
+      var position = new Vector3(GetJSONFloat(foods[i], "x"),
+                                                            100f,
+                                 GetJSONFloat(foods[i], "z")
+                                 );
+
+      if (!foodsDict.ContainsKey(id)) {
+        var food = Instantiate(foodPrefab, position, Quaternion.identity) as GameObject;
+        food.transform.parent = transform;
+        food.GetComponent<FoodController>().id = id;
+        foodsDict.Add(food.GetComponent<FoodController>().id, food);
+      } else {
+        ToggleState(id);
+      }
+    }
+  }
+
+  public void ToggleState (string id) {
+    objectState = foodsDict[id].activeSelf;
+    foodsDict[id].SetActive(!objectState);
+  }
+
+
+  float GetJSONFloat (JSONObject data, string key) {
+    return float.Parse(data[key].ToString().Replace("\"", ""));
+  }
+
+}
---
+++ Assets/Client Assets/Scripts/KnickKnackNetworkController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using SocketIO;
+
+public class KnickKnackNetworkController : MonoBehaviour {
+
+	// Use this for initialization
+  public GameObject foodPrefab;
+  public GameObject obstaclePrefab;
+  static SocketIOComponent socket;
+
+  void Awake () {
+    socket = NetworkController.socket;
+  }
+
+	void Start () {
+    socket.On("field_objects", CreateKnickknacks);
+    socket.On("eaten", ToggleFoodState);
+    socket.On("collided", ToggleObstacleState);
+	}
+
+  ////////////////////////////////////
+  //       Create Knickknacks       //
+  ////////////////////////////////////
+  void CreateKnickknacks (SocketIOEvent e) {
+    foodPrefab.GetComponent<FoodsController>().CreateFood(e.data["food"]);
+    obstaclePrefab.GetComponent<ObstaclesController>().CreateObstacle(e.data["obstacles"]);
+  }
+
+  ////////////////////////////////////
+  //     Methods Related to Food    //
+  ////////////////////////////////////
+  void ToggleFoodState (SocketIOEvent e) {
+    foodPrefab.GetComponent<FoodsController>().CreateFood(e.data);
+  }
+  public void FoodEaten (string id) {
+    socket.Emit("eat", new JSONObject(id));
+  }
+
+  ////////////////////////////////////
+  //  Methods Related to Obstacles  //
+  ////////////////////////////////////
+  void ToggleObstacleState (SocketIOEvent e) {
+    obstaclePrefab.GetComponent<ObstaclesController>().CreateObstacle(e.data);
+  }
+  public void ObstacleCollision (string id) {
+    socket.Emit("collision", new JSONObject(id));
+  }
+}

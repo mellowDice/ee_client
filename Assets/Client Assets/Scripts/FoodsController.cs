2ecaf5c826760a999f45dfc1a0e6c1b7362e7a6f
aaronwbrown <aaron.w.brown13@gmail.com>
2016-07-08 10:00:17 -0700

    Adding random colors to the food objects

--- Assets/Client Assets/Scripts/FoodsController.cs
+++ Assets/Client Assets/Scripts/FoodsController.cs
@@ -5,14 +5,16 @@ using System.Collections.Generic;
 public class FoodsController : MonoBehaviour {
 
   public GameObject foodPrefab;
+  Color[] colors = new Color[6];
   bool objectState;
   Dictionary<string, GameObject> foodsDict = new Dictionary<string, GameObject>();
 
+  public void Start() {
+    ChangeColor();
+  }
+
   public void CreateFood (JSONObject foods)
   {
-    // Debug.Log("foods" + foods);
-    // Debug.Log(foods[0]);
-    // Debug.Log(foods[0]["id"]);
     var length = foods.list.Count;
     for (var i = 0; i < length; i++) {
       var id = GetJSONString(foods[i], "id");
@@ -25,7 +27,9 @@ public class FoodsController : MonoBehaviour {
         var food = Instantiate(foodPrefab, position, Quaternion.identity) as GameObject;
         food.transform.parent = transform;
         food.GetComponent<FoodController>().id = id;
+        food.GetComponent<Renderer>().material.color = colors[Random.Range(0, colors.Length)];
         foodsDict.Add(food.GetComponent<FoodController>().id, food);
+
       } if (idVal < 0 && x <= 0 && z <= 0) {
         foodsDict[id].SetActive(false);
       } else {
@@ -36,6 +40,21 @@ public class FoodsController : MonoBehaviour {
     }
   }
 
+  // public void ToggleState (string id) {
+  //   objectState = foodsDict[id].activeSelf;
+  //   Debug.Log("state" + objectState);
+  //   foodsDict[id].SetActive(!objectState);
+  // }
+
+
+  public void ChangeColor() {
+    colors[0] = Color.cyan;
+    colors[1] = Color.red;
+    colors[2] = Color.green;
+    colors[3] = new Color(255, 165, 0);
+    colors[4] = Color.yellow;
+    colors[5] = Color.magenta;
+  }
   float GetJSONFloat (JSONObject data, string key) {
     return float.Parse(data[key].ToString().Replace("\"", ""));
   }
